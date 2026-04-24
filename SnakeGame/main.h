b8518c0d72cd4d54aa8f5e8a79b42b30692933a3
KinhKhaVN <pebin0123@gmail.com>
2026-04-24 23:05:54 +0700

    Define macro

---
+++ SnakeGame/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define WIDTH 1200
+#define HEIGHT 700
+#define FPS 60
+#define SNAKE_WIDTH 100
+#define SNAKE_HEIGHT 100
+#define CELL_SIZE 30
+#define CELL_COUNT 30
+#define OFF_SET 75
+
+
+
+#endif
