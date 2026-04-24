b0982416a71bb69fe8317887c07f833b282068d4
KinhKhaVN <pebin0123@gmail.com>
2026-04-24 19:18:49 +0700

    Ball object

---
+++ LuaGame2D/Pong/src/Ball.lua
@@ -0,0 +1,36 @@
+Ball = {}
+
+function Ball:init(x, y, width, height)
+  local obj = {}
+  setmetatable(obj, self)
+  self.__index = self
+  self.x = x
+  self.y = y
+  self.width = width
+  self.height = height
+
+  self.dx = math.random(2) == 1 and -100 or 100
+  self.dy = math.random(-50, 50)
+
+  return obj
+end
+
+function Ball:reset()
+  self.x = WINDOW_WIDTH / 2
+  self.y = WINDOW_HEIGHT / 2
+  self.dx = math.random(2) == 1 and -100 or 100
+  self.dy = math.random(-50, 50)
+end
+
+function Ball:update(dt)
+  self.x = self.x + self.dx * dt
+  self.y = self.y + self.dy * dt
+end
+
+function Ball:render()
+  love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+end
+
+
+
+
