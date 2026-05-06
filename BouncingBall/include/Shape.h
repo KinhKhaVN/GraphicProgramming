0c6d7357a755df7887d8544977ee7c1e69d3700c
KinhKhaVN <pebin0123@gmail.com>
2026-05-06 14:30:44 +0700

    Abstract class Shape

---
+++ BouncingBall/include/Shape.h
@@ -0,0 +1,27 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+
+#include "../include/raylib.h"
+#include "../include/main.h"
+
+class Shape{
+  public:
+    virtual void Update() = 0;
+    virtual void Render() = 0;
+    virtual void Move() = 0;
+    virtual void checkCollisionToBorder() = 0;
+    virtual bool isCollisionToTopBorder() = 0;
+    virtual bool isCollisionToBottomBorder() = 0;
+    virtual bool isCollisionToLeftBorder() = 0;
+    virtual bool isCollisionToRightBorder() = 0;
+
+  protected:
+    int x;
+    int y;
+    int dx;
+    int dy;
+};
+
+
+
+#endif
