96caa6d0ec9c51641d7c02c7e05bdbb69157d39a
KinhKhaVN <pebin0123@gmail.com>
2026-05-07 16:53:11 +0700

    Add colors to draw when collide

--- BouncingBall/include/main.h
+++ BouncingBall/include/main.h
@@ -1,8 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include "raylib.h"
 inline constexpr int WIDTH = 800;
 inline constexpr int  HEIGHT = 600;
 
+inline int countCollide = 1;
+inline Color colors[20] = { 
+    DARKGRAY, MAROON, ORANGE, DARKGREEN, DARKBLUE, DARKPURPLE, 
+    DARKBROWN, GRAY, RED, GOLD, LIME, BLUE, VIOLET, BROWN, 
+    PINK, YELLOW, GREEN, SKYBLUE, PURPLE, BEIGE 
+};
 
 #endif
