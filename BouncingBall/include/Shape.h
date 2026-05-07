1d2a903fe8b03938e3d818e3dfa81eef1ead3b3a
KinhKhaVN <pebin0123@gmail.com>
2026-05-07 16:53:27 +0700

    Add Handle collision

--- BouncingBall/include/Shape.h
+++ BouncingBall/include/Shape.h
@@ -9,17 +9,21 @@ class Shape{
     virtual void Update() = 0;
     virtual void Render() = 0;
     virtual void Move() = 0;
-    virtual void checkCollisionToBorder() = 0;
+    virtual void handleCollisionToBorder() = 0;
+    virtual bool isCollisionToBorder() = 0;
     virtual bool isCollisionToTopBorder() = 0;
     virtual bool isCollisionToBottomBorder() = 0;
     virtual bool isCollisionToLeftBorder() = 0;
     virtual bool isCollisionToRightBorder() = 0;
 
-  protected:
+  public:
+    double speed;
     int x;
     int y;
     int dx;
     int dy;
+    bool isCollided;
+    Color color = WHITE;
 };
 
 
