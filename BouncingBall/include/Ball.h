c7da8b47781e309be2282d25c36de707df49a549
KinhKhaVN <pebin0123@gmail.com>
2026-05-07 16:54:19 +0700

    Override function

--- BouncingBall/include/Ball.h
+++ BouncingBall/include/Ball.h
@@ -1,28 +1,32 @@
 #ifndef BALL_H
 #define BALL_H
 #include "Shape.h"
+#include "math.h"
 
 class Ball: public Shape{
   public:
     Ball(int _x, int _y, int _r):
       r(_r)
       {
-        x = _x;
-        y = _y;
-        dx = 2;
-        dy = 2;
+        x          = _x;
+        y          = _y;
+        dx         = 2;
+        dy         = 2;
+        speed      = 5;
+        isCollided = false;
       }
 
     void Update() override;
     void Render() override;
     void Move() override;
-    virtual void checkCollisionToBorder() override;
-    virtual bool isCollisionToTopBorder() override;
+    virtual void handleCollisionToBorder()   override;
+    virtual bool isCollisionToBorder()       override;
+    virtual bool isCollisionToTopBorder()    override;
     virtual bool isCollisionToBottomBorder() override;
-    virtual bool isCollisionToLeftBorder() override;
-    virtual bool isCollisionToRightBorder() override;
+    virtual bool isCollisionToLeftBorder()   override;
+    virtual bool isCollisionToRightBorder()  override;
 
-  private:
+  public:
     int r;
 };
 
